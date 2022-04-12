0795dc8869a0655e0be94086658c54fdf817e076
Marcus Soliai <marcussoliai@BB-MB-MS.local>
2022-04-12 02:03:21 -0500

    Test

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
+# visit http://127.0.0.1:8050/ in your web browser. Test
 
 from dash import Dash, html, dcc
 import plotly.express as px
